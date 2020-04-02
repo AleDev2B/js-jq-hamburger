5c8a5c370a8d3da9d52c2f293d57f2121911735e
Alessandro <alecritelli@gmail.com>
2020-04-02 02:44:20 +0200

    minor fix

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 $("i.fas").click( /* seleziono l'icona dell'hamburger e le dico di attivarsi al click*/
   function() {
-  $(".hamburger-menu").fadeIn(); /* esegui il comando apri il menu con effetto opacità per gradi*/
+  $(".hamburger-menu").fadeIn(); /* esegui il comando apri il menu con effetto opacità per gradi*/ OPPURE
+  // $("hamburger-menu").addClass("fadeIn");
 } )
 
 $("i.fas.fa-times").click( /* quando il menu è aperto e lo voglio chiudere seleziono l'icona X di chiusura*/
   function() {
-  $(".hamburger-menu").fadeOut(); /* e assegno l'istruzione di chiusura del menu con 'effetto opposto all'apertura*/
+  $(".hamburger-menu").fadeOut(); /* e assegno l'istruzione di chiusura del menu con 'effetto opposto all'apertura OPPURE*/
+  // $("hamburger-menu").removeClass("fadeIn");
 } )
